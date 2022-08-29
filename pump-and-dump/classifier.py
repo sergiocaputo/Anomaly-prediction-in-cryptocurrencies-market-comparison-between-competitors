91dc5b63f9e50539048f0708cea8d386f5bed262
Sergio Caputo <sergiocaputo1998@gmail.com>
2022-08-29 12:40:48 +0200

    Added pump and dump

---
+++ pump-and-dump/classifier.py
@@ -0,0 +1,173 @@
+import datetime
+import glob
+import os
+import re
+from time import time
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.metrics import confusion_matrix, f1_score, recall_score, precision_score
+from sklearn.model_selection import StratifiedKFold, cross_val_predict
+from sklearn.tree import DecisionTreeClassifier
+import joblib
+
+features_path = 'features/adapted_features/*csv'
+
+one_month_start = "2021-11-01 00:00:00"
+three_month_start = "2021-09-01 00:00:00"
+six_month_start = "2021-06-01 00:00:00"
+two_year_start = '2020-01-01 21:00:00'
+
+end_date = "2021-12-01 23:00:00"
+
+test_start_date = "2021-12-01"
+test_end_date = "2022-01-01"
+
+
+def create_training_testing_set(df, features, start_date_train, end_date_train, start_date_test, end_date_test):
+    X_train = df[(df['date'] >= pd.to_datetime(start_date_train)) & (df['date'] <= pd.to_datetime(end_date_train))][features]
+    Y_train = df[(df['date'] >= pd.to_datetime(start_date_train)) & (df['date'] <= pd.to_datetime(end_date_train))]['gt'].astype(int).values.ravel()
+    
+    X_test = df[(df['date'] >= pd.to_datetime(start_date_test)) & (df['date'] <= pd.to_datetime(end_date_test))][features]
+    Y_test = df[(df['date'] >= pd.to_datetime(start_date_test)) & (df['date'] <= pd.to_datetime(end_date_test))]['gt'].astype(int).values.ravel()
+    return X_train, Y_train, X_test, Y_test
+
+
+def train_random_forest(X_train, Y_train, X_test, Y_test, file, crypto, type):
+    clf = RandomForestClassifier(n_estimators=200, max_depth=5, random_state=1)
+    clf.fit(X_train, Y_train)
+    
+    Y_predictions = clf.predict(X_test)
+    #confusion_matrix = confusion_matrix(Y_test, Y_predictions)
+    #print(confusion_matrix)
+    
+    results = {'data' : os.path.split(file)[1],
+                'recall': [recall_score(Y_test, Y_predictions, average='macro')], 
+                'precision' : [precision_score(Y_test, Y_predictions, average='macro')],
+                'F1 score': [f1_score(Y_test, Y_predictions, average='macro')]}
+
+    if not os.path.exists('results/random_forest/'+ crypto + '/' + type):
+        os.makedirs('results/random_forest/'+ crypto + '/' + type)
+            
+    filename = 'results/random_forest/'+ crypto + '/' + type + '/' + os.path.splitext(os.path.basename(file))[0] + '.h5'
+    print(filename)
+    joblib.dump(clf, filename)
+
+    return pd.DataFrame(results)
+    
+
+def train_ada_boost(X_train, Y_train, X_test, Y_test, file, crypto, type):
+    clf = AdaBoostClassifier(DecisionTreeClassifier(max_depth=5), n_estimators=100, learning_rate=0.5, random_state=0)
+    clf.fit(X_train, Y_train)
+    
+    Y_predictions = clf.predict(X_test)
+    #confusion_matrix = confusion_matrix(Y_test, Y_predictions)
+    #print(confusion_matrix)
+    
+    results = {'data' : os.path.split(file)[1],
+                'recall': [recall_score(Y_test, Y_predictions, average='macro')], 
+                'precision' : [precision_score(Y_test, Y_predictions, average='macro')],
+                'F1 score': [f1_score(Y_test, Y_predictions, average='macro')]}
+
+    if not os.path.exists('results/ada_boost/'+ crypto + '/' + type):
+        os.makedirs('results/ada_boost/'+ crypto + '/' + type)
+
+    filename = 'results/ada_boost/'+ crypto + '/' + type + '/' + os.path.splitext(os.path.basename(file))[0] + '.h5'
+    print(filename)
+    joblib.dump(clf, filename)
+
+    return pd.DataFrame(results)
+
+
+def classifier(time_freq):
+
+    features = [#'std_rush_order',
+                #'avg_rush_order',
+                'std_trades',
+                'std_volume',
+                'avg_volume',
+                'std_price',
+                'avg_price',
+                'avg_price_max',
+                'hour_sin',
+                'hour_cos',
+                'minute_sin',
+                'minute_cos']
+
+    cryptos = list(os.listdir('autoencoder_dataset/adapted'))
+    #cryptos = list(set([((os.path.splitext(i)[0].split('_')[0])) for i in os.listdir('features/adapted_features')]))
+    cryptos.append('all')
+
+    # results for random forest
+    one_month_results_rf = pd.DataFrame()
+    three_month_results_rf = pd.DataFrame()
+    six_month_results_rf = pd.DataFrame()
+    two_year_results_rf = pd.DataFrame()
+    
+    # results for ada boost
+    one_month_results_ada = pd.DataFrame()
+    three_month_results_ada = pd.DataFrame()
+    six_month_results_ada = pd.DataFrame()
+    two_year_results_ada = pd.DataFrame()
+
+    for c in cryptos:
+        crypto = os.path.splitext(os.path.basename(c))[0]
+        print(crypto)
+        
+        for rolling_freq in [24, 30, 
+        #168, 
+        720]:
+            file = 'features/adapted_features/' + crypto + "_" + time_freq + "_" + str(rolling_freq) +'.csv'
+            print(file)
+            computed_data = pd.read_csv(file, parse_dates=['date'])
+
+            # one month training
+            print('One month training')
+            X_train_1m, Y_train_1m, X_test, Y_test = create_training_testing_set(computed_data, features, one_month_start, end_date, test_start_date, test_end_date)
+            results = train_random_forest(X_train_1m, Y_train_1m, X_test, Y_test, file, crypto, type='one_month')
+            one_month_results_rf = pd.concat([one_month_results_rf, results])
+            results = train_ada_boost(X_train_1m, Y_train_1m, X_test, Y_test, file, crypto, type='one_month')
+            one_month_results_ada = pd.concat([one_month_results_ada, results])
+
+            
+            # three months training
+            print('Three months training')
+            X_train_3m, Y_train_3m, X_test, Y_test = create_training_testing_set(computed_data, features, three_month_start, end_date, test_start_date, test_end_date)
+            results = train_random_forest(X_train_3m, Y_train_3m, X_test, Y_test, file, crypto, type='three_months')
+            three_month_results_rf = pd.concat([three_month_results_rf, results])
+            results = train_ada_boost(X_train_3m, Y_train_3m, X_test, Y_test, file, crypto, type='three_months')
+            three_month_results_ada = pd.concat([three_month_results_ada, results])
+
+           
+            # six months training
+            print('Six months training')
+            X_train_6m, Y_train_6m, X_test, Y_test = create_training_testing_set(computed_data, features, six_month_start, end_date, test_start_date, test_end_date)
+            results = train_random_forest(X_train_6m, Y_train_6m, X_test, Y_test, file, crypto, type='six_months')
+            six_month_results_rf = pd.concat([six_month_results_rf, results])
+            results = train_ada_boost(X_train_6m, Y_train_6m, X_test, Y_test, file, crypto, type='six_months')
+            six_month_results_ada = pd.concat([six_month_results_ada, results])
+            
+
+            # two years training
+            print('Two years training')
+            X_train_2y, Y_train_2y, X_test, Y_test = create_training_testing_set(computed_data, features, two_year_start, end_date, test_start_date, test_end_date)
+            results = train_random_forest(X_train_2y, Y_train_2y, X_test, Y_test, file, crypto, type="two_years")
+            two_year_results_rf = pd.concat([two_year_results_rf, results])
+            results = train_ada_boost(X_train_6m, Y_train_6m, X_test, Y_test, file, crypto, type="two_years")
+            two_year_results_ada = pd.concat([two_year_results_ada, results])
+
+
+        one_month_results_rf.to_csv('results/one_month_rf.csv', index=False, float_format='%.3f')
+        three_month_results_rf.to_csv('results/three_months_rf.csv', index=False, float_format='%.3f')
+        six_month_results_rf.to_csv('results/six_months_rf.csv', index=False, float_format='%.3f')
+        two_year_results_rf.to_csv('results/two_years_rf.csv', index=False, float_format='%.3f')
+
+        one_month_results_ada.to_csv('results/one_month_ada.csv', index=False, float_format='%.3f')
+        three_month_results_ada.to_csv('results/three_months_ada.csv', index=False, float_format='%.3f')
+        six_month_results_ada.to_csv('results/six_months_ada.csv', index=False, float_format='%.3f')
+        two_year_results_ada.to_csv('results/two_years_ada.csv', index=False, float_format='%.3f')
+
+
+if __name__ == '__main__':
+    start = datetime.datetime.now()
+    classifier(time_freq='4H')
+    print(datetime.datetime.now() - start)
---
+++ pump-and-dump/features.py
@@ -0,0 +1,238 @@
+from ctypes import resize
+import glob
+import datetime
+import os
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+from collections import Counter
+
+path_original = 'autoencoder_dataset/original/*.csv'
+path_adapted = 'autoencoder_dataset/adapted/*.csv'
+
+
+def std_rush_order_feature(df_buy, time_freq, rolling_freq):
+    df_buy = df_buy.groupby(df_buy.index).count()
+    #print(df_buy)
+    df_buy[df_buy == 1] = 0
+    df_buy[df_buy > 1] = 1
+    buy_volume = df_buy.groupby(pd.Grouper(freq=time_freq))['btc_volume'].sum()
+    buy_count = df_buy.groupby(pd.Grouper(freq=time_freq))['btc_volume'].count()
+    buy_volume.drop(buy_volume[buy_count == 0].index, inplace=True)
+    buy_volume.dropna(inplace=True)
+    rolling_diff = buy_volume.rolling(window=rolling_freq).std()
+    results = rolling_diff.pct_change()
+    #print(len(results))
+    return results
+
+
+def avg_rush_order_feature(df_buy, time_freq, rolling_freq):
+    df_buy = df_buy.groupby(df_buy.index).count()
+    df_buy[df_buy == 1] = 0
+    df_buy[df_buy > 1] = 1
+    buy_volume = df_buy.groupby(pd.Grouper(freq=time_freq))['btc_volume'].sum()
+    buy_count = df_buy.groupby(pd.Grouper(freq=time_freq))['btc_volume'].count()
+    buy_volume.drop(buy_volume[buy_count == 0].index, inplace=True)
+    buy_volume.dropna(inplace=True)
+    rolling_diff = buy_volume.rolling(window=rolling_freq).mean()
+    results = rolling_diff.pct_change()
+    #print(len(results))
+    return results
+
+
+def std_trades_feature(df_buy_rolling, rolling_freq):
+    buy_volume = df_buy_rolling['price'].count()
+    buy_volume.drop(buy_volume[buy_volume == 0].index, inplace=True)
+    buy_volume.dropna(inplace=True)
+    rolling_diff = buy_volume.rolling(window=rolling_freq).std()
+    results = rolling_diff.pct_change()
+    #print(len(results))
+
+    return results
+
+
+def std_volume_feature(df_buy_rolling, rolling_freq):
+    buy_volume = df_buy_rolling['btc_volume'].sum()
+    buy_volume.drop(buy_volume[buy_volume == 0].index, inplace=True)
+    buy_volume.dropna(inplace=True)
+    rolling_diff = buy_volume.rolling(window=rolling_freq).std()
+    results = rolling_diff.pct_change()
+    #print(len(results))
+    return results
+
+
+def avg_volume_feature(df_buy_rolling, rolling_freq):
+    buy_volume = df_buy_rolling['btc_volume'].sum()
+    buy_volume.drop(buy_volume[buy_volume == 0].index, inplace=True)
+    buy_volume.dropna(inplace=True)
+    rolling_diff = buy_volume.rolling(window=rolling_freq).mean()
+    results = rolling_diff.pct_change()
+    #print(len(results))
+    return results
+
+
+def std_price_feature(df_buy_rolling, rolling_freq):
+    buy_volume = df_buy_rolling['price'].mean()
+    buy_volume.dropna(inplace=True)
+    rolling_diff = buy_volume.rolling(window=rolling_freq).std()
+    results = rolling_diff.pct_change()
+    #print(len(results))
+    return results
+
+
+def avg_price_feature(df_buy_rolling, rolling_freq):
+    buy_volume = df_buy_rolling['price'].mean()
+    buy_volume.dropna(inplace=True)
+    rolling_diff = buy_volume.rolling(window=rolling_freq).mean()
+    results = rolling_diff.pct_change()
+    #print(len(results))
+    return results
+
+
+def avg_price_max(df_buy_rolling, rolling_freq):
+    buy_volume = df_buy_rolling['price'].max()
+    buy_volume.dropna(inplace=True)
+    rolling_diff = buy_volume.rolling(window=rolling_freq).mean()
+    results = rolling_diff.pct_change()
+    #print(len(results))
+    return results
+
+
+def chunks_time(df_buy_rolling):
+    # compute any kind of aggregation
+    buy_volume = df_buy_rolling['price'].max()
+    buy_volume.dropna(inplace=True)
+    #the index contains time info
+    return buy_volume.index
+
+def majority_voting(df_buy_rolling):
+    majority_labels = []
+
+    for key, item in df_buy_rolling:
+        gt_labels = []
+
+        gt_labels.append(df_buy_rolling.get_group(key)['gt'].values)
+        
+        counts = Counter(gt_labels[0])
+        most_frequent = counts.most_common(1)
+        majority_labels.append(most_frequent[0][0])
+
+    #print(majority_labels, len(majority_labels))
+    
+    return majority_labels
+
+
+def build_features(file, coin, time_freq, rolling_freq, index):
+    df = pd.read_csv(file)
+    df["datetime"] = pd.to_datetime(df['datetime'])
+    df = df.reset_index().set_index('datetime')
+    
+    df_buy = df[df['side'] == 'buy']
+
+    df_buy_grouped = df_buy.groupby(pd.Grouper(freq=time_freq)) #raggruppa per ogni 4 ore
+    date = chunks_time(df_buy_grouped)
+    
+    results_df = pd.DataFrame(
+        {'date': date,
+         'pump_index': index,
+         #'std_rush_order': std_rush_order_feature(df_buy, time_freq, rolling_freq).values,
+         #'avg_rush_order': avg_rush_order_feature(df_buy, time_freq, rolling_freq).values,
+         'std_trades': std_trades_feature(df_buy_grouped, rolling_freq).values,
+         'std_volume': std_volume_feature(df_buy_grouped, rolling_freq).values,
+         'avg_volume': avg_volume_feature(df_buy_grouped, rolling_freq).values,
+         'std_price': std_price_feature(df_buy_grouped, rolling_freq).values,
+         'avg_price': avg_price_feature(df_buy_grouped, rolling_freq).values,
+         'avg_price_max': avg_price_max(df_buy_grouped, rolling_freq).values,
+         'hour_sin': np.sin(2 * np.pi * date.hour/23),
+         'hour_cos': np.cos(2 * np.pi * date.hour/23),
+         'minute_sin': np.sin(2 * np.pi * date.minute / 59),
+         'minute_cos': np.cos(2 * np.pi * date.minute / 59),
+         'gt': majority_voting(df_buy_grouped),
+         })
+    results_df['symbol'] = coin
+    
+    for i in range(results_df.shape[0]):
+        if results_df['gt'].iloc[i] == 2:
+            results_df['gt'].iloc[i] = 1
+    results_df.replace(np.inf, np.nan, inplace=True)
+
+    return results_df.dropna()
+    
+
+
+def build_features_multi(time_freq, rolling_freq):
+
+    files = glob.glob(path_adapted)
+
+    all_results_df = pd.DataFrame()
+    count = 0
+
+    if not os.path.exists('features/adapted_features'):
+                os.makedirs('features/adapted_features')
+                
+    for f in files:
+        print(f, time_freq, rolling_freq)
+        coin = os.path.splitext(os.path.basename(f))[0]
+
+        results_df = build_features(f, coin, time_freq, rolling_freq, count)
+        
+        results_df.to_csv('features/adapted_features/{}_{}_{}.csv'.format(coin, time_freq, rolling_freq), index=False, float_format='%.3f')
+        all_results_df = pd.concat([all_results_df, results_df])
+        count += 1
+
+    all_results_df.to_csv('features/adapted_features/all_{}_{}.csv'.format(time_freq, rolling_freq), index=False, float_format='%.3f')
+
+
+def compute_features():
+    # time step and time window
+    # 4h cause curve shifting applied in costruction phase for dataset
+    # window of elements considered (24 (1day), 168 (1 week), 720 (1 month))
+
+    #aggiungere 30 rolling freq
+
+    build_features_multi(time_freq='4H', rolling_freq=24)
+    build_features_multi(time_freq='4H', rolling_freq=30) #solo 
+    build_features_multi(time_freq='4H', rolling_freq=168)
+    build_features_multi(time_freq='4H', rolling_freq=720)
+
+
+def adapt_features():
+    files = glob.glob(path_original)
+
+    useless_features = ["Open","High","Low","Adj_Close","sentiment","VWAP","SMA_14","SMA_21","SMA_5","SMA_12","SMA_26","SMA_13","SMA_30","SMA_20","SMA_50","SMA_100","SMA_200",
+    "EMA_14","EMA_21","EMA_5","EMA_12","EMA_26","EMA_13","EMA_30","EMA_20","EMA_50","EMA_100","EMA_200","RSI_14","RSI_21","RSI_5","RSI_12",
+    "RSI_26","RSI_13","RSI_30","RSI_20","RSI_50","RSI_100","RSI_200","MACD_12_26_9","MACDH_12_26_9","MACDS_12_26_9","BBL_20","BBM_20",
+    "BBU_20","MOM","STOCHF_14","STOCHF_3","STOCH_5","STOCH_3","CMO","DPO","UO","lag_1"]
+
+    features = ["symbol","timestamp","datetime","side","price","amount","btc_volume","gt"]
+
+    if not os.path.exists('autoencoder_dataset/adapted'):
+                os.makedirs('autoencoder_dataset/adapted')
+
+    for f in files:
+        print(f)
+        df = pd.read_csv(f)
+
+        df = df.drop(useless_features, axis=1)
+        
+        crypto = os.path.splitext(os.path.basename(f))[0]
+        df['symbol'] = crypto
+        df = df.rename(columns={'Date' : 'datetime', 'Close' : 'price', 'Volume' : 'btc_volume', 'anomaly' : 'gt'})
+        for i in range(df.shape[0]):
+            df.loc[i, 'timestamp'] = datetime.timestamp(datetime.strptime( df.loc[i, 'datetime'], '%Y-%m-%d %H:%M:%S'))
+            df.loc[i, 'amount'] = df.loc[i, 'btc_volume'] / df.loc[i, 'price']
+        df['side'] = 'buy'
+        
+        df = df[features]
+        df.to_csv('autoencoder_dataset/adapted/{}.csv'.format(crypto), index=False)
+    
+
+
+if __name__ == '__main__':
+    start = datetime.now()
+
+    adapt_features()
+    compute_features()
+    
+    print(datetime.now() - start)
